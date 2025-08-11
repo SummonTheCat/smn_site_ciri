f71e4565030307911ae48df848163cad2900856e
SummonTheCat <julien_andrew@outlook.com>
2025-08-11 02:55:19 +0200

    Remove background image component and update HTML files to include background image directly in development, game design, index, and technical art pages.

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ async fn main() {
 
     components.register(plugins::plugin_components::components::comp_header::CompHeader);
     components.register_simple("./components/underConstruction.html");
-    components.register_simple("./components/background.html");
 
     manager.apply_plugin(Box::new(components));
     manager.apply_plugin(Box::new(PluginStatic::new(true, vec!["html".to_string(), "pdf".to_string(), "svg".to_string()])));
